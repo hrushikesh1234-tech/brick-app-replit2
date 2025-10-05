98f276b2f0d0ed4e6ed29c8e47bddcb1461f1794
hrushikesh1234-tech <hrushikeshmore954@gmail.com>
2025-10-05 18:26:55 +0530

    Update vite.config.ts     allowables added

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 5000,
+     allowedHosts: [
+      "localhost",
+      "*.replit.dev", // allow all Replit preview domains
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
